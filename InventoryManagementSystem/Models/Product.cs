48c786e11a2418e96a6da0c90ca944cc025d36fc
Mumtaz Aisha <baryaisha06@gmail.com>
2025-06-13 22:05:30 +0530

    Product and Stock  Movement models

---
+++ InventoryManagementSystem/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class Product
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int? SupplierId { get; set; }
+    }
+}
---
+++ InventoryManagementSystem/Models/StockMovement.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagement.Models
+{
+    public class StockMovement
+    {
+        public int MovementId { get; set; }
+        public string ProductSKU { get; set; }
+        public string MovementType { get; set; } // 'add', 'remove', 'adjust'
+        public int QuantityChanged { get; set; }
+        public DateTime MovementTime { get; set; }
+        public string Notes { get; set; }
+    }
+}
