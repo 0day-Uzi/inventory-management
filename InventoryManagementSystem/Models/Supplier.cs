e320bff7d0c0c6ae72b6cef31ec1f75872fefa73
Uzman Badurdeen <187439757+0day-Uzi@users.noreply.github.com>
2025-06-13 18:37:39 +0530

    SUpplier Model

---
+++ InventoryManagementSystem/Models/Supplier.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
