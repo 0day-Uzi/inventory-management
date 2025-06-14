da868d65820a44f8dd90b93d1a7ae0d952f57ac0
Piyumlee <cb013215@students.apiit.lk>
2025-06-14 10:20:09 +0530

    Order Details Form

---
+++ InventoryManagementSystem/OrderDetailsForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class OrderDetailsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridView dgvItems;
+        private System.Windows.Forms.Button btnClose;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.dgvItems = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Text = "Order Details";
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+
+            // 
+            // dgvItems
+            // 
+            this.dgvItems.AllowUserToAddRows = false;
+            this.dgvItems.AllowUserToDeleteRows = false;
+            this.dgvItems.ReadOnly = true;
+            this.dgvItems.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvItems.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvItems.Location = new System.Drawing.Point(20, 60);
+            this.dgvItems.Size = new System.Drawing.Size(540, 280);
+
+            // 
+            // btnClose
+            // 
+            this.btnClose.Text = "Close";
+            this.btnClose.Size = new System.Drawing.Size(80, 30);
+            this.btnClose.Location = new System.Drawing.Point(480, 360);
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+
+            // 
+            // OrderDetailsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 410);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.dgvItems);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "OrderDetailsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Order Details";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ InventoryManagementSystem/OrderDetailsForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using InventoryManagementSystem.Controllers;
+
+namespace InventoryManagementSystem
+{
+    public partial class OrderDetailsForm : Form
+    {
+        private readonly int orderId;
+        private readonly OrderController controller;
+        private readonly ProductController productController;
+
+        public OrderDetailsForm(int orderId)
+        {
+            InitializeComponent();
+            this.orderId = orderId;
+            controller = new OrderController();
+            productController = new ProductController();
+            LoadOrderItems();
+        }
+
+        private void LoadOrderItems()
+        {
+            var items = controller.GetItemsByOrderId(orderId);
+            var products = productController.GetAllProducts();
+
+            var displayList = items.Select(i => new
+            {
+                i.ProductSKU,
+                ProductName = products.FirstOrDefault(p => p.SKU == i.ProductSKU)?.Name ?? "(Unknown)",
+                i.Quantity,
+                i.PriceEach,
+                Total = i.Quantity * i.PriceEach
+            }).ToList();
+
+            dgvItems.DataSource = displayList;
+
+            if (dgvItems.Columns["ProductSKU"] != null)
+                dgvItems.Columns["ProductSKU"].HeaderText = "SKU";
+            if (dgvItems.Columns["ProductName"] != null)
+                dgvItems.Columns["ProductName"].HeaderText = "Product";
+            if (dgvItems.Columns["Quantity"] != null)
+                dgvItems.Columns["Quantity"].HeaderText = "Qty";
+            if (dgvItems.Columns["PriceEach"] != null)
+                dgvItems.Columns["PriceEach"].HeaderText = "Unit Price";
+            if (dgvItems.Columns["Total"] != null)
+                dgvItems.Columns["Total"].HeaderText = "Line Total";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
