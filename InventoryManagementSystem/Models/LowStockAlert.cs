98a9344a7524da44cca938924b77ebea972658cc
unknown <Nelakashenal@gmail.com>
2025-06-14 10:50:57 +0530

    Low Stock Alert Model

---
+++ InventoryManagementSystem/Models/LowStockAlert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Models
+{
+    public class LowStockAlert
+    {
+        public int AlertID { get; set; }
+        public string ProductSku { get; set; }
+        public DateTime AlertTime { get; set; }
+        public int CurrentQuantity { get; set; }
+    }
+}
