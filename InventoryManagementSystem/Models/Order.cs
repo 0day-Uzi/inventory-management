c5901b651a5789a8cfc0465b693a04a2d01f0624
Piyumlee <cb013215@students.apiit.lk>
2025-06-14 10:12:09 +0530

    Orders and Order Items models

---
+++ InventoryManagementSystem/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InventoryManagementSystem.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int? SupplierId { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public string OrderStatus { get; set; }
+    }
+}
---
+++ InventoryManagementSystem/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public string ProductSKU { get; set; }
+        public int Quantity { get; set; }
+        public decimal PriceEach { get; set; }
+    }
+}
