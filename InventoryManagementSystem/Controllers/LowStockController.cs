dedfd3aef296457fbd27962eefe7e1019e2e32d4
unknown <Nelakashenal@gmail.com>
2025-06-14 10:53:25 +0530

    Low Stock Controller

---
+++ InventoryManagementSystem/Controllers/LowStockController.cs
@@ -0,0 +1,26 @@
+﻿using InventoryManagementSystem.Models;
+using InventoryManagementSystem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class LowStockController
+    {
+
+        private readonly LowStockAlertRepository lowStockAlertRepository;
+
+        public LowStockController()
+        {
+            lowStockAlertRepository = new LowStockAlertRepository();
+        }
+
+        public List<LowStockAlert> GetAllAlerts()
+        {
+            return lowStockAlertRepository.GetAllAlerts();
+        }
+    }
+}
